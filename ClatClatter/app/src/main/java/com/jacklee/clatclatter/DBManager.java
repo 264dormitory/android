7df6ef043089c4223978817bb7c2db5038ccd837
高黎明 <xp01255410@icloud.com>
2018-04-13 00:00:34 +0800

    初始化数据库

--- ClatClatter/app/src/main/java/com/jacklee/clatclatter/DBManager.java
+++ ClatClatter/app/src/main/java/com/jacklee/clatclatter/DBManager.java
@@ -1,39 +1,66 @@
 package com.jacklee.clatclatter;
 
 import android.content.Context;
+import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
 
 /**
- * 初始化数据库文件
+ * 数据库对象
  * Created by liming on 18-4-11.
  */
 
-public class DBManager {
-    private static final String TAG = DBManager.class.getSimpleName();
-    private   static SQLiteDatabase database;
-    public static  final String DATABASE_FILENAME = "app_info.db";
-    public static final String PACKAGE_NAME = "com.jacklee.clatclatter";
-    public static final String DATABASE_PATH = "/data" + Environment.getDataDirectory()
-                                                .getAbsolutePath() + PACKAGE_NAME;
+public class DBManager extends SQLiteOpenHelper {
+    private final String TAG = DBManager.class.getSimpleName();
 
-    public static SQLiteDatabase openDatabase(Context context) {
-        try {
-            //打开数据库文件
-            String databaseName = DATABASE_PATH + "/" + DATABASE_FILENAME;
-            File file = new File(databaseName);
+    private final String CREATE_TASK_TABLE =
+            "create table task(" +
+            "id integer primary key AUTOINCREMENT," +
+            "title varchar(20) NOT NULL," +
+            "mark varchar(255)," +
+            "focus char(1) default 0," +
+            "is_repeat char(1) default 0," +
+            "sleep_pattern char(1) default 0," +
+            "repeat_pattern integer default 0," +
+            "priority char(10)," +
+            "sleep_pattern_kind integer," +
+            "start_time datetime," +
+            "end_time datetime," +
+            "remind_time datetime," +
+            "task_date date)";
 
-            if (!file.exists()) {
-                Log.i(TAG, "数据库文件不存在,创建数据库");
-                File dir = new File(DATABASE_PATH);
+    private final String CREATE_APPBACKLIST_TABLE =
+            "create table app_backlist(" +
+                    "name char(10) primary key" +
+                    ")";
 
-                if (!dir.exists()) {
-                    Log.i(TAG, "创建数据库的目录");
-                    dir.mkdir();
-                }
-        }
+
+    public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    /**
+     * 创建数据库的时候回调该函数
+     * @param sqLiteDatabase
+     */
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.i(TAG, "创建task表");
+        sqLiteDatabase.execSQL(CREATE_TASK_TABLE);
+        Log.i(TAG, "创建app_backlist表");
+        sqLiteDatabase.execSQL(CREATE_APPBACKLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }
