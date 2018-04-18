17cc2a09be34df7f1fd710b9207e7941583accf4
高黎明 <xp01255410@icloud.com>
2018-04-18 23:29:06 +0800

    修改保存数据的方法

--- ClatClatter/app/src/main/java/com/jacklee/clatclatter/database/app_backlist.java
+++ ClatClatter/app/src/main/java/com/jacklee/clatclatter/database/app_backlist.java
@@ -1,10 +1,12 @@
 package com.jacklee.clatclatter.database;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * 李世杰创建app_backlist类，对应相应的数据库中task表的映射
  */
 
-public class app_backlist {
+public class app_backlist extends DataSupport {
 
     private String name;  //应用名称
 
